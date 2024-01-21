ac52be3bea4c0129997c97b7ed653bd9ad563884
Nihar Ghevaria <niharghevaria48@gmail.com>
2024-01-21 18:44:33 -0500

    Changed id in component-card.ts file

--- src/app/content-card/content-card.component.ts
+++ src/app/content-card/content-card.component.ts
@@ -23,7 +23,7 @@ export class ContentCardComponent {
       tags: ["PVP","Multiplayer","Competitive"],
     });
     this.contentList.add({
-      id: 1,
+      id: 2,
       title: 'Red Dead Redemption2',
       description: 'RDR2 is the epic tale of outlaw Arthur Morgan and the infamous Van der Linde gang, on the run across America at the dawn of the modern age.',
       creator: 'Rockstar',
@@ -32,7 +32,7 @@ export class ContentCardComponent {
       tags: ["Open World","Singleplayer","Online"],
     });
     this.contentList.add({
-      id: 1,
+      id: 3,
       title: 'Sea of Thieves',
       description: 'Celebrate five years since Sea of Thieves launch with this special edition, including a copy of the game with all permanent content added since launch, plus a 10,000 gold bonus and a selection of Hunter cosmetics: the Hunter Cutlass, Pistol, Compass, Hat, Jacket and Sails.',
       creator: 'Xbox Game Studios',
